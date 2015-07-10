21f5f097bbc273482f8b01684b7ab96b4db8bfc4
Chris Stebe <cstebe@iserv4u.com>
2015-07-10 12:37:10 +0200

    initial commit

---
+++ Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace dmstr\modules\redirect;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'vendor\dmstr\modules\redirect\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
