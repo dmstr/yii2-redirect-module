46c720cef3bc1adeb1f9ef11e4deb30bd3cf181f
Chris Stebe <cstebe@iserv4u.com>
2015-07-10 14:51:55 +0200

    updated Module

--- Module.php
+++ Module.php
@@ -1,12 +1,31 @@
 <?php
-
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2015 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace dmstr\modules\redirect;
 
 class Module extends \yii\base\Module
 {
+    /**
+     * Controller namespace
+     * @var string
+     */
     public $controllerNamespace = '\dmstr\modules\redirect\controllers';
 
+    /**
+     * Crud message catalogue for \Yii::t()
+     * @var string
+     */
+    public $messageCatalogue = 'dmstr';
 
+
+    /**
+     * @inheritdoc
+     */
     public function init()
     {
         parent::init();
