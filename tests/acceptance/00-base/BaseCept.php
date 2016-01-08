352da3783c1663e2318b31910150d4b7c57b2dd5
Chris Stebe <cstebe@iserv4u.com>
2016-01-08 15:44:41 +0100

    updated test and config

--- tests/acceptance/00-base/BaseCept.php
+++ tests/acceptance/00-base/BaseCept.php
@@ -7,8 +7,10 @@ $loginPage = LoginPage::openBy($I);
 $I->see('Sign in', 'h3');
 $I->amGoingTo('try to login with correct credentials');
 $loginPage->login('admin', 'admin');
-$I->amGoingTo('try to view and create pages');
-$I->amOnPage('/redirects');
 
+$I->amGoingTo('try to view redirects');
+$I->amOnPage('/redirects');
+$I->wait(1);
 $I->see('Redirects', 'h1');
+
 $I->makeScreenshot('success-redirects-index');
