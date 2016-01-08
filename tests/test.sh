5efe8851e5fa7914fcaf5b329fb93555db8b9b2e
Chris Stebe <cstebe@iserv4u.com>
2016-01-08 13:28:37 +0100

    updated test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,7 @@ export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-testredirects}
 docker-compose --x-networking up -d & wait
 docker-compose --x-networking ps
 
-docker-compose --x-networking run php sh /app/src/init.sh
+docker-compose --x-networking run php sh /app/src/run.sh
 
+docker-compose --x-networking run php codecept build
 docker-compose --x-networking run php codecept run -c /app/vendor/dmstr/yii2-redirect-module/codeception.yml
