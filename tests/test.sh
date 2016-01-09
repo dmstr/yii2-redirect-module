bc5c0e73a845f36fd228af2d92a49129a4e5953e
Chris Stebe <cstebe@iserv4u.com>
2016-01-09 01:51:44 +0100

    Revert "verbose"
    This reverts commit e1e5e3168ffbd5097aa99b9154cf19997a401903.

--- tests/test.sh
+++ tests/test.sh
@@ -9,4 +9,4 @@ docker-compose --x-networking ps
 
 docker-compose --x-networking run php sh /app/src/init.sh
 
-docker-compose --x-networking run php codecept run -c -v /app/vendor/dmstr/yii2-redirect-module/codeception.yml
+docker-compose --x-networking run php codecept run -c /app/vendor/dmstr/yii2-redirect-module/codeception.yml
