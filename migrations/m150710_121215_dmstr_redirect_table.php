06176b1bf60fc781be3318f91ece13d8b28e8ea8
Tobias Munk <schmunk@usrbin.de>
2017-04-03 15:56:58 +0200

    updated migrations to support postgres

--- migrations/m150710_121215_dmstr_redirect_table.php
+++ migrations/m150710_121215_dmstr_redirect_table.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link http://www.diemeisterei.de/
  * @copyright Copyright (c) 2014 diemeisterei GmbH, Stuttgart
@@ -6,8 +7,27 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-class m150710_121215_dmstr_redirect_table extends \dmstr\db\mysql\FileMigration
+class m150710_121215_dmstr_redirect_table extends \yii\db\Migration
 {
-    public $file = 'sql/m150710_121215_dmstr_redirect_table.sql';
+
+    public function up()
+    {
+        $this->createTable('dmstr_redirect',
+            [
+                'id' => $this->primaryKey(),
+                'type' => $this->string(10),
+                'from_domain' => $this->string(255)->null(),
+                'to_domain' => $this->string(255)->null(),
+                'from_path' => $this->string(255)->null(),
+                'to_path' => $this->string(255)->null(),
+                'status_code' => $this->integer(3)->notNull(),
+                'created_at' => $this->dateTime(),
+                'updated_at' => $this->dateTime(),
+            ]);
+    }
+
+    public function down()
+    {
+        return false;
+    }
 }
